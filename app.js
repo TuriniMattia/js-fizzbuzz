1ea02401d84dd746e386b146a3fec16d28a1407f
TuriniMattia <turinimattia96@gmail.com>
2024-03-06 15:57:31 +0100

    [add]dichiarato variabile

---
+++ app.js
@@ -0,0 +1,15 @@
+// Consegna:
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+// per i multipli di 3 stampi “Fizz” al posto del numero e
+// per i multipli di 5 stampi “Buzz” al post del numero.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+//dichiare una variabile
+    for (let i = 0; i < 100; i++){
+        let num = i + 1;
+    }
+        
+//stampare in console
+//SE è un multiplo di 3 , stamperà Fizz
+//SE ALTRIMENTI è un multimo di 5 stamperà Buzz
+//ALTRIME
